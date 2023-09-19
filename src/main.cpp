dfd2c46eba61bcb0d110e92bd31f4fdc9254ed3e
Maksim <bondarenko.maksim@agsr.by>
2023-09-19 17:45:22 +0300

    [+] base vulkan init

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,105 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+
+class TriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+
+private:
+    void initWindow() {
+        int rv = glfwInit();
+        if (rv != GLFW_TRUE) {
+            std::cerr << "Failed to init GLFW" << std::endl;
+            return;
+        }
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window_ = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+        if (!window_) {
+            std::cerr << "Failed to create window!" << std::endl;
+        }
+    }
+
+    void initVulkan() {
+        enumExtensions();
+        createInstance();
+    }
+
+    void enumExtensions() {
+        uint32_t extension_count = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+        std::vector<VkExtensionProperties> extensions(extension_count);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.data());
+
+        std::cout << "Available extensions: " << std::endl;
+        for (const auto& extension : extensions) {
+            std::cout << "\t" << extension.extensionName << std::endl;
+        }
+    }
+
+    void createInstance() {
+        VkApplicationInfo app_info{};
+        app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        app_info.pApplicationName = "Triangle";
+        app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        app_info.pEngineName = "No Engine";
+        app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        app_info.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo create_info{};
+        create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        create_info.pApplicationInfo = &app_info;
+
+        uint32_t glfw_extensions_count = 0;
+        const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extensions_count);
+        create_info.ppEnabledExtensionNames = glfw_extensions;
+        create_info.enabledExtensionCount = glfw_extensions_count;
+        create_info.enabledLayerCount = 0; 
+
+        VkResult res = vkCreateInstance(&create_info, nullptr, &instance_);
+        if (res != VK_SUCCESS) {
+            std::cerr << "error: " << res << std::endl;
+            throw std::runtime_error("Failed to create VK instance!");
+        }
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window_)) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        vkDestroyInstance(instance_, nullptr);
+        glfwDestroyWindow(window_);
+        glfwTerminate();
+    }
+
+private:
+    GLFWwindow* window_ = nullptr;
+    VkInstance instance_;
+};
 
 int main()
 {
-    return 0;
+    TriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
